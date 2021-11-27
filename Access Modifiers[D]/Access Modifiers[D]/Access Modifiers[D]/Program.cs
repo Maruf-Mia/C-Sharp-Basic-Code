0ce21c61c6e4fd8e8e61998741fb8756b2ab7b88
Mustafizur Rahman Maruf <58640382+Maruf84@users.noreply.github.com>
2021-11-27 12:30:34 +0600

    Add files via upload

---
+++ Access Modifiers[D]/Access Modifiers[D]/Access Modifiers[D]/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Access_Modifiers_D_
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    //person person = new person();
+        //    //person.name = "";
+        //    //person.address = "";
+        //    //person.bloodgroup = "";
+
+        //    Person person = new Person();
+        //    Person.name = "";
+        //    Person.address = "";
+        //    Person.bloodGroup = "";
+
+        //}
+    }
+}
+
---
+++ Access Modifiers[D]/Access Modifiers[D]/MyLibrary/Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Access_Modifiers_D_;
+namespace MyLibrary
+{
+    public class Account
+    {
+        //public Account()
+        //{
+        //    Person person = new Person();
+        //    person.name = "";
+        //}
+
+        public Account()
+          {
+            Person person = new Person();
+            person.name = "";
+          }
+    }
+}
