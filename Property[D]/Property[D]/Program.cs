945528579017a929cb8797cf2060d692a5137270
Mustafizur Rahman Maruf <58640382+Maruf84@users.noreply.github.com>
2021-11-27 12:15:04 +0600

    Add files via upload

---
+++ Property[D]/Property[D]/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Property_D_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.SetName("Shakib");
+            Console.WriteLine(student.GetName());
+            student.Name = "Shakib";
+            student.Id = "18-yyyyy-1";
+            student.Cgpa = 3.5f;
+            student.University = "AIUB";
+            Console.WriteLine(student.Name);
+            Console.WriteLine(student.Id);
+            student.PrintStudent();
+        }
+    }
+}
